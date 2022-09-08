9debdbe93628ba4e70317bfd69213b39323a8736
Eoghan O'S <eoghan@sullie.dev>
2022-09-08 20:44:37 +0100

    Add basic search logic

--- src/compnents/BodyGrid.js
+++ src/compnents/BodyGrid.js
@@ -1,17 +1,42 @@
 import { Container, Grid, GridItem, Text } from "@chakra-ui/react";
+import { useState } from "react";
 import MealCard from "./MealCard";
 import SearchField from "./SearchField";
 
 function BodyGrid() {
+  const [search, setSearch] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const searchFunction = () => {
+    if (!search && searchTerm.length > 0) {
+      setSearch(!search);
+      console.log(searchTerm);
+    }
+    if (search === true) {
+      setSearch(!search);
+    }
+  };
+
+  const updateSearchedTerm = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
     <Grid templateColumns="repeat(5, 1fr)" gap={4}>
       <GridItem colSpan={2} h="20">
-        <SearchField />
+        <SearchField
+          searchFunction={searchFunction}
+          searched={search}
+          updateSearchedTerm={updateSearchedTerm}
+        />
       </GridItem>
       <GridItem colStart={4} colEnd={6} h="100">
         <Container m={25}>
-          <Text>No meals looked up</Text>
-          <MealCard />
+          {search ? (
+            <MealCard />
+          ) : (
+            <Text>Search for a meal by an ingredient</Text>
+          )}
         </Container>
       </GridItem>
     </Grid>
--- src/compnents/SearchField.js
+++ src/compnents/SearchField.js
@@ -1,11 +1,23 @@
 import React from "react";
 import { Button, Container, Input } from "@chakra-ui/react";
 
-export default function SearchField() {
+export default function SearchField(props) {
   return (
     <Container m={25}>
-      <Input placeholder="Saerch using an ingredient" />
-      <Button mt={1}>Search</Button>
+      <Input
+        placeholder="Saerch using an ingredient"
+        onChange={props.updateSearchedTerm}
+      />
+      <Button mt={1} onClick={props.searchFunction}>
+        Search
+      </Button>
+      {props.searched ? (
+        <Button mt={1} mx={1} onClick={props.searchFunction}>
+          Reset
+        </Button>
+      ) : (
+        <></>
+      )}
     </Container>
   );
 }
